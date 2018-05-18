39a04cf9131c9cd6eee154fb3a063f550e35b074
Russ Beuker <russbeuker@gmail.com>
2018-05-17 21:34:18 -0700

    Added separate functions for test and train.

--- gen_digit_pngs.py
+++ gen_digit_pngs.py
@@ -13,7 +13,7 @@ def logmsg(msg=''):
     s = f'{datetime.datetime.today().strftime(fmt)}: {msg}'
     print(s)
 
-def plot_mnist_digits_packed(x, y, max_length=100, filter=None, show=False, save=True, show_index=False):
+def plot_mnist_digits_packed(x, y, title='title', max_length=100, filter=None, show=False, save=True, show_index=False):
     # this will plot each digit into a .png that is 10 columns wide
     # x - this is the x_train data (features) in shape ex. 60000,784, datatype=uint8, pixel values 0 to 255
     # y - this is the y_train data (labels) in shape ex. 60000,1
@@ -48,37 +48,58 @@ def plot_mnist_digits_packed(x, y, max_length=100, filter=None, show=False, save
         ax1.set_yticklabels([])
         ax1.set_aspect('equal')
         # if there is a filter, put the index number in red.  Otherwise, put the digit label in blue
-        if filter == None:
-            label = y[i]
-            plt.text(1, 5, label, color='red')
-        else:
-            if show_index:
+        if y is not None:
+            if filter == None:
                 label = y[i]
-                plt.text(1, 5, label, color='blue')
+                plt.text(1, 5, label, color='red')
+            else:
+                if show_index:
+                    label = y[i]
+                    plt.text(1, 5, label, color='blue')
         plt.imshow(x[i], cmap=plt.get_cmap('gray_r'), vmax=255)
         frame1 = plt.gca()
         frame1.axes.get_xaxis().set_visible(False)
         frame1.axes.get_yaxis().set_visible(False)
     if save:
-        s = f'.//input//digits//digit_{str(filter)}.png'
+        s = f'.//input//digits//{title}.png'
         plt.savefig(s)
     if show:
         plt.show()
 
-def main():
-    #l oad the mnist data.  In this example we already have it stored in .npy's
+def gen_train_pngs():
+    #load the mnist data.  In this example we already have it stored in .npy's
     # data must be sent to the plotting function as shape(rows, 784), datatype=uint8, unnormalized(pixel range from 0 to 255)
     xfile = './/input//x_train.npy'
     yfile = './/input//y_train.npy'
-    x_train = np.load(file=xfile)
-    y_train = np.load(file=yfile)
-    perfile = x_train.shape[0]
-    #perfile=100  #set this to a small number to test. It takes a LONG time to process an entire mnist dataset
+    x = np.load(file=xfile)
+    y = np.load(file=yfile)
+    perfile = x.shape[0]
+    perfile=100  #set this to a small number to test. It takes a LONG time to process an entire mnist dataset
     logmsg('Generating a separate .png for each digit in x_train.')
     for i in range(0, 10):
         logmsg(str(i) + '...')
-        plot_mnist_digits_packed(x_train, y_train, max_length=perfile, filter=i, show=False, save=True, show_index=True)
+        plot_mnist_digits_packed(x, y, title=f'train_{i}', max_length=perfile, filter=i, show=False, save=True, show_index=True)
     logmsg('Done.')
+
+def gen_test_pngs():
+    #load the mnist data.  In this example we already have it stored in .npy's
+    # data must be sent to the plotting function as shape(rows, 784), datatype=uint8, unnormalized(pixel range from 0 to 255)
+    xfile = './/input//x_test.npy'
+    x = np.load(file=xfile)
+    y = None
+    perfile = x.shape[0]
+    perfile=4000  #set this to a small number to test. It takes a LONG time to process an entire mnist dataset
+    logmsg('Generating a separate .png for each digit in x_train.')
+    for i in range(0, x.shape[0], perfile):
+        logmsg(str(i) + '...')
+        plot_mnist_digits_packed(x[i:i + perfile], y, title=f'test_{i}', max_length=perfile, filter=None, show=False, save=True, show_index=False)
+    logmsg('Done.')
+
+def main():
+    # gen_train_pngs()
+    gen_test_pngs()
+
+
 
 if __name__ == "__main__":
     main()
